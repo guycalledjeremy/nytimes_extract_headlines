e0df4d41504d5e1b2571264991cf9824f794c3b8
guycalledjeremy <bitbucket@cwsl.ca>
2019-02-23 22:34:25 -0500

    Added topics extraction

--- extract_headlines.py
+++ extract_headlines.py
@@ -42,8 +42,8 @@ def main(args):
     print("Begin file extraction from URL...")
     # Use a smaller time slot for testing:
     # for year in range(1900, 2001):
-    # TODO: try get around the api's request limit (which is around 40/min)
-    for year in range(1990, 2001):
+    # TODO: try get around the api's request limit (which is around 20/min)
+    for year in range(1995, 2000):
         random.shuffle(months)
         for i in range(2):
             with requests.get('https://api.nytimes.com/svc/archive/v1/'+str(year)+'/'+str(months[i])+'.json?api-key='+api_key, headers=headers) as file:
@@ -59,12 +59,16 @@ def main(args):
                         article_out["headline"]["main"] = article["headline"]["main"]
                         # TODO: Notice not all years have the same json file structure.
                         # Pending: What's our solution to these two sections?
-                        #try:
-                        #    article_out["headline"]["print_headline"] = article["headline"]["print_headline"]
-                        #    article_out["headline"]["sub"] = article["headline"]["sub"]
-                        #except KeyError:
-                        #    print('Older news having different format, /'+str(year)+'/'+str(months[i]))
+                        # try:
+                        #     article_out["headline"]["print_headline"] = article["headline"]["print_headline"]
+                        #     article_out["headline"]["sub"] = article["headline"]["sub"]
+                        # except KeyError:
+                        #     print('Older news having different format, /'+str(year)+'/'+str(months[i]))
                         article_out["keywords"] = article["keywords"]
+                        # Add the topics to a list to be recorded in another json file
+                        for item in article["keywords"]:
+                            if item["name"] == "subject" and item["value"] not in topicOutput:
+                                topicOutput.append(item["value"])
                         article_out["pub_date"] = article["pub_date"]
                         article_out["news_desk"] = article["news_desk"]
                         article_out["section_name"] = article["section_name"]
@@ -77,19 +81,27 @@ def main(args):
     print("File extracted from URL...")
 
     if finished:
-        # Create the new
-        f_out = open(args.output, 'w')
+        # Create the new output topic json file
+        t_out = open(args.output_t, 'w')
+        t_out.write(json.dumps(topicOutput))
+        t_out.close()
+        print("Topic JSON file created...")
+        # Create the new output data json file
+        f_out = open(args.output_d, 'w')
         f_out.write(json.dumps(allOutput))
         f_out.close()
-        print("JSON file created...")
+        print("Data JSON file created...")
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process each .')
-    parser.add_argument("-o", "--output", help="Directs the output to a \
+    parser.add_argument("-o", "--output_d", help="Directs the output to a \
+    filename of your choice", required=True)
+    parser.add_argument("-t", "--output_t", help="Directs the outputed topics to a \
     filename of your choice", required=True)
     args = parser.parse_args()
 
     print("Headline Extraction Started...")
     main(args)
+    print("Headline Extraction Finished.")
