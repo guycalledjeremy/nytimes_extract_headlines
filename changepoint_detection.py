6d4e725bdcd89a722507b0d7b17f922db723645b
Samantha Quinto <samantha.quinto@mail.utoronto.ca>
2019-03-27 17:53:29 -0400

    fixed error

--- changepoint_detection.py
+++ changepoint_detection.py
@@ -11,14 +11,17 @@ def cpd(freq_count):
     years = np.arange(0, 101, 1)
     mse = []
     for i in years:
-        fit = np.polyfit(years[i:], profile[i:], 2)
+        fit = np.polyfit(years[i:], freq_count[i:], 2)
         linefit = np.poly1d(fit)
         y_fit = np.zeros(i)
         y_fit = np.concatenate((y_fit, linefit(years[i:])), axis=0)
-        mse.append(mean_squared_error(profile, y_fit))
+        plt.plot(years, y_fit)
+        mse.append(mean_squared_error(freq_count, y_fit))
     min_mse = mse.index(min(mse))
     if min_mse == 100:
         cp_year = 2000
     else:
         cp_year = 1900 + min_mse
     return cp_year
+
+   
