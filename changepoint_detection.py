c46e4b3e4e6bed8f6bab02b7e5c23bc83120eded
guycalledjeremy <bitbucket@cwsl.ca>
2019-03-28 13:00:47 -0400

    Modified the file a bit

--- changepoint_detection.py
+++ changepoint_detection.py
@@ -1,27 +1,22 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
+
 def cpd(freq_count):
     """
     :param freq_count: the frequency of a word from 1900-2000
     :type freq_count: list of int
     :return: cp_year, the changepoint year
-    :rtype: int
+    :rtype: int(the year), float(the min mse value)
     """
-    years = np.arange(0, 101, 1)
+    years = np.arange(0, 100, 1)
     mse = []
     for i in years:
         fit = np.polyfit(years[i:], freq_count[i:], 2)
         linefit = np.poly1d(fit)
         y_fit = np.zeros(i)
         y_fit = np.concatenate((y_fit, linefit(years[i:])), axis=0)
-        plt.plot(years, y_fit)
         mse.append(mean_squared_error(freq_count, y_fit))
     min_mse = mse.index(min(mse))
-    if min_mse == 100:
-        cp_year = 2000
-    else:
-        cp_year = 1900 + min_mse
-    return cp_year
-
-   
+    cp_year = 1900 + min_mse
+    return cp_year, mse[min_mse]
